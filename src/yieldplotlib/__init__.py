5c1d56a1c9355152d4650263fd519a34581fc2bb
Corey Spohn <corey.a.spohn@nasa.gov>
2024-08-13 15:29:32 -0400

    feat: Added base Plot class (#9)
    * feat: Add stubs from pleaserender
    
    * chore: Clean up plot class
    
    * feat: Add utility functions

--- src/yieldplotlib/__init__.py
+++ src/yieldplotlib/__init__.py
@@ -1,5 +1,6 @@
 """yieldplotlib - A library for plotting yield data."""
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "calculate_axis_limits_and_ticks", "get_nice_number"]
 
 from ._version import __version__
+from .util import calculate_axis_limits_and_ticks, get_nice_number
