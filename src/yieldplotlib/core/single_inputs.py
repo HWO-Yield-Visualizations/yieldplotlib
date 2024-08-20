40079ce2491c24da61f3221a17f7874103e62614
Sarah Steiger <ssteiger@stsci.edu>
2024-08-20 15:07:13 -0400

    fix: update check_units function to gracefully handle non iterables and to give a helpful message if all checks pass.

--- src/yieldplotlib/core/single_inputs.py
+++ src/yieldplotlib/core/single_inputs.py
@@ -22,17 +22,22 @@ class SingleInput(dict):
     def check_units(self, key, desired_unit):
         """Checks that all values of key have the appropriate desired unit."""
         try:
-            for value in self.get(key):
-                if value.unit != desired_unit:
-                    raise AssertionError(f"Value {value} for {key} does not have desired "
+            iterator = iter(self.get(key))
+            try:
+                for value in iterator:
+                    if value.unit != desired_unit:
+                        raise AssertionError(f"Value {value} for {key} does not have desired "
+                                             f"unit {desired_unit}")
+            except AttributeError:
+                raise AttributeError(f'{key} does not have a value of type astropy.units.Quantity')
+
+        except TypeError:
+            try:
+                if self.get(key).unit != desired_unit:
+                    raise AssertionError(f"Value {self.get(key)} for {key} does not have desired "
                                          f"unit {desired_unit}")
-        except AttributeError:
-            raise AttributeError(f'{key} does not have a value of type astropy.units.Quantity')
+            except AttributeError:
+                raise AttributeError(f'{key} does not have a value of type astropy.units.Quantity')
 
-
-
-
-
-
-
-
+        finally:
+            print('All unit checks passed.')
