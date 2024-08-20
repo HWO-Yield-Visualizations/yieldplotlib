d7be64505998fa34ae9c769915648e372491281c
Corey Spohn <cas584@cornell.edu>
2024-08-20 15:26:34 -0400

    feat: Add logger

--- src/yieldplotlib/__init__.py
+++ src/yieldplotlib/__init__.py
@@ -1,6 +1,12 @@
 """yieldplotlib - A library for plotting yield data."""
 
-__all__ = ["__version__", "calculate_axis_limits_and_ticks", "get_nice_number"]
+__all__ = [
+    "__version__",
+    "logger",
+    "calculate_axis_limits_and_ticks",
+    "get_nice_number",
+]
 
 from ._version import __version__
+from .logger import logger
 from .util import calculate_axis_limits_and_ticks, get_nice_number
