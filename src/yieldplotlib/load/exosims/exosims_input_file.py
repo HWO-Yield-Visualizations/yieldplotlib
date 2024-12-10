5b7a4d92977c4e59f078c125e5edb8454a842675
Sarah Steiger <ssteiger@stsci.edu>
2024-12-10 10:04:42 -0500

    chore: only return parameters for instruments/modes that are used.

--- src/yieldplotlib/load/exosims/exosims_input_file.py
+++ src/yieldplotlib/load/exosims/exosims_input_file.py
@@ -4,6 +4,16 @@ from pathlib import Path
 
 from yieldplotlib.core.file_nodes import JSONFile
 
+# Define which nested keys correspond to the modes, systems, and instruments for parsing.
+# Could also be added as a flag in the key_map
+INSTRUMENT_KEYS = ['qe', 'cic', 'read_noise', 'dark_current', 'pixel_scale', 'texp', 'optics_throughput']
+INSTRUMENT_KEYS += ['sc_' + k for k in INSTRUMENT_KEYS]
+
+MODE_KEYS = ['obs_lam', 'snr']
+MODE_KEYS += ['sc_' + k for k in MODE_KEYS]
+
+SYSTEM_KEYS = ['coron_lam', 'iwa', 'owa', 'bw', 'optics']
+
 
 class EXOSIMSInputFile(JSONFile):
     """Node for handling the EXOSIMS input JSON files."""
@@ -16,15 +26,31 @@ class EXOSIMSInputFile(JSONFile):
     def get(self, key: str):
         """Custom logic for the input JSON files."""
         # TODO: Implement custom logic for these files.
+        # Define which instruments, systems and modes are used. Will not return values for unused
+        # but defined modes/instruments/systems.
+        used_modes = super().get('modes')
+        used_instruments = [m["instName"] for m in used_modes]
+        used_systems = [m["systName"] for m in used_modes]
+
         values = super().get(key)
-        if isinstance(values, dict):
-            if key.startswith('sc'):
+        # If only a single value, return.
+        if not isinstance(values, dict):
+            return values
+
+        else:
+            if key in INSTRUMENT_KEYS or MODE_KEYS:
+                if key.startswith('sc'):  # indicates spectroscopy parameter.
+                    for k in values.copy().keys():
+                        if 'spectro' not in k or k not in used_instruments:
+                            del values[k]
+                else:
+                    for k in values.copy().keys():
+                        if 'spectro' in k or k not in used_instruments:
+                            del values[k]
+
+            if key in SYSTEM_KEYS:
                 for k in values.copy().keys():
-                    if 'spectro' not in k:
-                        del values[k]
-            else:
-                for k in values.copy().keys():
-                    if 'spectro' in k:
+                    if k not in used_systems:
                         del values[k]
 
         return values
