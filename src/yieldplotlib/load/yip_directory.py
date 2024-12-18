ef9bcd995ddf547a351505569623f7536e6e3f85
Sarah Steiger <ssteiger@stsci.edu>
2024-12-18 11:44:43 -0500

    chore: update YIPDirectory to use yippy.

--- src/yieldplotlib/load/yip_directory.py
+++ src/yieldplotlib/load/yip_directory.py
@@ -4,42 +4,42 @@
 from pathlib import Path
 from yieldplotlib.core import DirectoryNode, Node
 from yieldplotlib.core.file_nodes import FitsFile
+from yippy.coronagraph import Coronagraph
+import astropy.io.fits as pyfits
 
 
 class YIPDirectory(DirectoryNode):
     """Loader for yield input packages, organizing files into a directory-based structure."""
 
     def __init__(self, root_directory: Path):
-        """Initialize the EXOSIMSLoader by scanning the directory structure."""
+        """Initialize the loader by scanning the directory structure."""
         super().__init__(root_directory)
+        self.coronagraph = Coronagraph(root_directory)
 
     def _create_directory_node(self, path: Path) -> Node:
-        """Override directory node creation logic for EXOSIMS-specific directories."""
+        """Override directory node creation logic for YIP-specific directories."""
         return self.create_base_directory(path)
 
     def _create_file_node(self, path: Path) -> Node:
-        """Override file node creation logic for EXOSIMS-specific files."""
+        """Override file node creation logic for YIP-specific files."""
         if path.suffix == ".fits":
             return FitsFile(path)
         else:
             return self.create_base_file(path)
 
     def get(self, key: str):
-        """Search for a key (e.g., star_distances) in the tree structure."""
-        for file in self._children:
-            if (file.file_name == "offax_psf.fits"
-                    and key == "offax_psf"):
-                return file.get("data")
-            elif (file.file_name == "offax_psf_offset_list.fits"
-                  and key == "offax_psf_offset_list"):
-                return file.get("data")
-            elif (file.file_name == "sky_trans.fits"
-                  and key == "sky_trans"):
-                return file.get("data")
-            elif (file.file_name == "stellar_intens.fits"
-                  and key == "stellar_intens"):
-                return file.get("data")
-            elif (file.file_name == "stellar_intens_diam_list.fits"
-                  and key == "stellar_intens_diam_list"):
-                return file.get("data")
-        return None
+        """Search for a key (e.g., "data" or "D") in the tree structure."""
+        if key.endswith(".data"):
+            if key == "offax.data":
+                return pyfits.getdata(Path(self.coronagraph.yip_path, "offax_psf.fits"))
+            elif key == "offax_offset_list.data":
+                return pyfits.getdata(Path(self.coronagraph.yip_path, "offax_psf_offset_list.fits"))
+            elif key == "stellar_intens.data":
+                return pyfits.getdata(Path(self.coronagraph.yip_path, "stellar_intens.fits"))
+            elif key == "stellar_intens_diam_list.data":
+                return pyfits.getdata(Path(self.coronagraph.yip_path, "stellar_intens_diam_list.fits"))
+            elif key == "sky_trans.data":
+                return pyfits.getdata(Path(self.coronagraph.yip_path, "sky_trans.fits"))
+        else:
+            return getattr(self.coronagraph, key)
+
