d6bab9f3543f16e6f4c59ac4095a3a44d44df6c6
Sarah Steiger <ssteiger@stsci.edu>
2025-01-06 15:16:29 -0500

    chore: remove forgotten print statement.

--- src/yieldplotlib/plots/yip_plots.py
+++ src/yieldplotlib/plots/yip_plots.py
@@ -103,7 +103,6 @@ def plot_core_throughtput(
         plt.style.use("cyberpunk")
         prop_cycle = plt.rcParams['axes.prop_cycle']
         colors = prop_cycle.by_key()['color']
-        print(colors)
         custom_cycler = (cycler(linestyle=['-', '--', ':', '-.']) +
                          cycler(color=colors[:4]))
         plt.rc('axes', prop_cycle=custom_cycler)
@@ -141,5 +140,3 @@ def plot_core_throughtput(
 
     if title:
         plt.title(title)
-
-    plt.show()
