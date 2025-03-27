11201a8efe1093419099aa5bb4e524d709a6b770
Corey Spohn <cas584@cornell.edu>
2025-03-27 15:27:41 -0400

    chore: Add nox config file

---
+++ noxfile.py
@@ -0,0 +1,20 @@
+"""Nox sessions."""
+
+import nox
+
+PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12"]
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def tests(session):
+    """Run the test suite with pytest."""
+    # Install all test dependencies
+    session.install(".[test]")
+    # Run the tests
+    session.run(
+        "pytest",
+        "--cov=yieldplotlib",
+        "--cov-report=term",
+        "--cov-report=html",
+        *session.posargs,
+    )
