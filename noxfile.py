cb8868afa2706c06c8ab91222018966491925672
Corey Spohn <cas584@cornell.edu>
2025-03-28 19:31:50 -0400

    chore: Update to >= 3.10

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@
 import nox
 
 
-@nox.session(venv_backend="uv", python=["3.10", "3.11", "3.12"])
+@nox.session(venv_backend="uv", python=["3.10", "3.11", "3.12", "3.13"])
 def tests(session):
     """Run the test suite with pytest."""
     # Install all test dependencies
