f8fcd1b10a849d33f8f7a557181f2e82e80b9fad
Corey Spohn <cas584@cornell.edu>
2025-03-29 20:44:53 -0400

    chore: Adding the jupyter_execute folder to ignore list

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "jupyter_execute"]
 
 language = "Python"
 
